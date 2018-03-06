9d26f6c5477d252f7cb8a0c6a3700aa9134d53d2
Chidi Emeh <chidiemeh184@gmail.com>
2018-03-05 19:44:18 -0500

    Filters VC button, sliders and steppers added

--- Liq/Filters/FiltersVC.swift
+++ Liq/Filters/FiltersVC.swift
@@ -10,17 +10,52 @@ import UIKit
 
 class FiltersVC: UIViewController {
 
+    @IBOutlet weak var priceSliderLabel: UILabel!
+    @IBOutlet weak var alcoholSliderLabel: UILabel!
+    @IBOutlet weak var volumeSliderLabel: UILabel!
+    @IBOutlet weak var returnBatchLabel: UILabel!
+    @IBOutlet weak var addFilterbutton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addFilterbutton.layer.cornerRadius = addFilterbutton.layer.frame.height/2
 
     }
 
+    @IBAction func priceSliderChanged(_ sender: UISlider) {
+        let priceValue = Double(sender.value)
+        priceSliderLabel.text = String(format: "$%.2f", priceValue)
+    }
+    
+    @IBAction func alcoholSliderChanged(_ sender: UISlider) {
+
+        let alcoholValue = Float(sender.value)
+        
+        alcoholSliderLabel.text = "\(String(format: "%.1f", alcoholValue))%"
+    }
+    
+    @IBAction func volumeSliderChanged(_ sender: UISlider) {
+        let volumeValue = Int(sender.value)
+        volumeSliderLabel.text = "\(volumeValue)ml"
+    }
+    
+    
     @IBAction func closeButtonTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
     
     
 
-
+    @IBAction func addFiltersButtonTapped(_ sender: UIButton) {
+        //Create and send filters
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func returnBatchStepperChanged(_ sender: UIStepper) {
+        let stepperValue = Int(sender.value)
+        returnBatchLabel.text = "\(stepperValue)"
+    }
+    
+    
 
 }
